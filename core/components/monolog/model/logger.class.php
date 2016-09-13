952873caf9c64a7887a849dbd3022f507b7cf578
Romain Tripault <romain@melting-media.com>
2016-09-13 11:17:55 +0200

    The service now complies with LoggerInterface

--- core/components/monolog/model/logger.class.php
+++ core/components/monolog/model/logger.class.php
@@ -7,7 +7,42 @@ use Melting\MODX\Logger\Service;
 /**
  * A log service to help ship logs to Monolog
  */
-class Logger extends Service
+class Logger extends Service implements \Psr\Log\LoggerInterface
 {
-
+    public function emergency($message, array $context = [])
+    {
+        $this->getLogger()->emergency($message, $context);
+    }
+    public function alert($message, array $context = [])
+    {
+        $this->getLogger()->alert($message, $context);
+    }
+    public function critical($message, array $context = [])
+    {
+        $this->getLogger()->critical($message, $context);
+    }
+    public function error($message, array $context = [])
+    {
+        $this->getLogger()->error($message, $context);
+    }
+    public function warning($message, array $context = [])
+    {
+        $this->getLogger()->warning($message, $context);
+    }
+    public function notice($message, array $context = [])
+    {
+        $this->getLogger()->notice($message, $context);
+    }
+    public function info($message, array $context = [])
+    {
+        $this->getLogger()->info($message, $context);
+    }
+    public function debug($message, array $context = [])
+    {
+        $this->getLogger()->debug($message, $context);
+    }
+    public function log($level, $message, array $context = [])
+    {
+        $this->getLogger()->log($level, $message, $context);
+    }
 }
